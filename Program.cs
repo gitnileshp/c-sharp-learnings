721c8fa8dcc18bd9cbaf053df5749ece4e79ec72
gitnileshp <90727844+gitnileshp@users.noreply.github.com>
2021-09-14 17:45:43 -0400

    Manual upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace c_sharp_learnings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Test");
+        }
+    }
+}
